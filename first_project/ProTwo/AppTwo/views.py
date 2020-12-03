7e48789a9aec54d01fde695cddda5cb72136ed6e
sumit <mandal.sumit00@gmail.com>
2020-12-04 00:57:02 +0530

    new 00:56 4-12

--- first_project/ProTwo/AppTwo/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    return HttpResponse("<em>My second project</em>")
-def help(request):
-    helpdict={'help_insert':'HELP PAGE'}
-    return render(request,'Apptwo/help.html',context=helpdict)
