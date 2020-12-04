0224d64c1e89895aaef6a8ef0b3f86320c2646eb
sumit <mandal.sumit00@gmail.com>
2020-12-04 20:07:17 +0530

    20:07/04-12

--- first_project/ProTwo/ProTwo/urls.py
+++ first_project/ProTwo/ProTwo/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
 from appTwo import views
 
 urlpatterns = [
+    url(r'^help/',include('appTwo.urls')),
     url(r'^$',views.index,name='index'),
     url(r'^admin/', admin.site.urls),
 ]
--- first_project/ProTwo/appTwo/views.py
+++ first_project/ProTwo/appTwo/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<em>My second Project</em>")
+
+def help(request):
+    helpdict={'help_insert':'HELP PAGE'}
+    return render(request,'appTwo/help.html',context=helpdict)
