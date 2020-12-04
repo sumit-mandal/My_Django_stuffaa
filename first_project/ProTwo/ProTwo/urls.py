150ca5d6356a8cd3b79af3567b9459619d86723c
sumit <mandal.sumit00@gmail.com>
2020-12-04 18:58:01 +0530

    4-12,18:57

--- first_project/ProTwo/ProTwo/urls.py
+++ first_project/ProTwo/ProTwo/urls.py
@@ -1,4 +1,4 @@
-"""first_project URL Configuration
+"""ProTwo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from first_app import views
+from appTwo import views
 
 urlpatterns = [
     url(r'^$',views.index,name='index'),
