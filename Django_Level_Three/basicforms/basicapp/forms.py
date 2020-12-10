aecaad765ac109744a9f279f8ed794038fac845d
sumit <mandal.sumit00@gmail.com>
2020-12-10 11:05:49 +0530

    11:05/ 10-12

--- Django_Level_Three/basicforms/basicapp/forms.py
+++ Django_Level_Three/basicforms/basicapp/forms.py
@@ -5,13 +5,13 @@ from django.core import validators
 class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
+    verify_email = forms.EmailField(label='Enter your email again:')
     text = forms.CharField(widget=forms.Textarea)
-    botcatcher = forms.CharField(required = False,
-                                widget=forms.HiddenInput,
-                                validators=[validators.MaxLengthValidator(0)])
 
-    def clean_botcatcher(self):
-        botcatcher = self.cleaned_data['botcatcher']
-        if len(botcatcher) > 0:
-            raise forms.ValidationError("Gotcha Bot! ")
-        return botcatcher
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vmail = all_clean_data['verify_email']
+
+        if email != vmail:
+            raise forms.ValidationError("MAKE SURE EMAILS MATCH!")
