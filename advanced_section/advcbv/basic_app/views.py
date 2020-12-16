3bdb72840b447fc63203845758ef27127ecc2a29
sumit <mandal.sumit00@gmail.com>
2020-12-16 12:28:18 +0530

    12:28 16-12

--- advanced_section/advcbv/basic_app/views.py
+++ advanced_section/advcbv/basic_app/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import View,TemplateView
+from django.views.generic import View,TemplateView,ListView,DetailView
+from . import models
 
-
-class IndexView(TemplateView):
+class  IndexView(TemplateView):
     template_name = 'index.html'
 
-    def get_context_data(self,**kwargs):
-        context = super().get_context_data(**kwargs)
-        context['inject_me']='Basic Injection'
-        return context
+
+class SchoolListView(ListView):
+    context_object_name = 'schools'
+    model = models.School
+    #ListView changes name of models.School to small letter and ands _list thus returning school_list
+
+class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
+    model = models.School
+    template_name = 'basic_app/School_detail.html'
+    # DetailView only returns model lowercase
